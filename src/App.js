9a3340a15297bc2853d67f2b0f70cab9ed7d8052
Sebastian Gallese <sgallese@Sebastians-MacBook-Pro.local>
2019-04-13 11:36:06 -0700

    Add Search API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    bosses: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://demo3062747.mockable.io/search/?query=name`)
+      .then(res => {
+        const bosses = res.data;
+        this.setState({bosses});
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,6 +32,13 @@ class App extends Component {
           >
             Learn React
           </a>
+          <p>
+            Searches from API:
+          </p>
+          <p>
+            {this.state.bosses.map((boss) => boss.name)}
+            // other attributes include boss.phot_url and boss.profile_url
+          </p>
         </header>
       </div>
     );
